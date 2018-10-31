0a8606d7d254fd3491df0600e60cce3cfdf7f86b
Alex Saveliev <savelievaleks@hotmail.com>
2018-10-31 02:04:23 +0200

    nelmio/cors

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     SunCat\MobileDetectBundle\MobileDetectBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
